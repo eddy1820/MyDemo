9d30671cd0ecb7c7000ea33b9a3d30c963fa7fbb
eddy1820 <eddy1820@gmail.com>
2022-05-06 18:20:09 +0800

    update test page

---
+++ app/src/androidTest/java/com/example/mydemo/main/core/test/Test2ActivityTest.kt
@@ -0,0 +1,17 @@
+package com.example.mydemo.main.core.test
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class Test2ActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
